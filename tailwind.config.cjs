33f3d6037abe9ee6876c76357a3300c7e038ee2a
Yerayzl1 <yerayzafra1@gmail.com>
2025-09-29 07:44:26 +0200

    Instalando TailwindCSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  // Debemos indicar donde se va a procesar el CSS para que luego lo compile.
+  content: ['./views/**/*.pug'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
