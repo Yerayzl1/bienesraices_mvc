00a47169cfafa7fe4b21ead1aff3282b6153e78b
Yerayzl1 <yerayzafra1@gmail.com>
2025-10-07 12:17:36 +0200

    Creando la página de olvide mi password

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -10,6 +10,12 @@ const formularioRegistro = (req, res) => {
   });
 };
 
+const formularioOlvidePassword = (req, res) => {
+  res.render('auth/olvide-password', {
+    pagina: 'Recupera tu acceso a Bienes Raices',
+  });
+};
+
 // export default exporta solo un objeto, cuando los importas, puedes usar el nombre que quieras.
 // export y llaves exporta múltiples funciones, cuando los importas, tienes que usar el mismo nombre que la función a la que llamas.
-export { formularioLogin, formularioRegistro };
+export { formularioLogin, formularioRegistro, formularioOlvidePassword };
