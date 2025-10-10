7e1741490272eab6de10dd379dfd4336f0deec33
Yerayzl1 <yerayzafra1@gmail.com>
2025-10-10 19:48:25 +0200

    Ocultando la información de la base de datos en variables de entorno

--- config/db.js
+++ config/db.js
@@ -1,19 +1,26 @@
 import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env' });
 
-const db = new Sequelize('bienesraizes_node_mvc', 'root', 'yeray', {
-  host: 'localhost',
-  port: 3306,
-  dialeg: 'mysql',
-  define: {
-    timestamps: true,
-  },
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-  operatorAliases: false,
-});
+const db = new Sequelize(
+  process.env.DB_NOMBRE,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: 3306,
+    dialeg: 'mysql',
+    define: {
+      timestamps: true,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    operatorAliases: false,
+  }
+);
 
 export default db;
